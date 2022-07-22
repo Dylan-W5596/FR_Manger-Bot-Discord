eb3b39836e159dc49981734a6ccd246f80a13734
Dylan-W5596 <108519539+Dylan-W5596@users.noreply.github.com>
2022-07-22 17:47:59 +0800

    Update. Join and leave

--- bot.py
+++ bot.py
@@ -6,8 +6,18 @@ bot = commands.Bot(command_prefix="FR!")  #這個機器人
 
 @bot.event
 async def on_ready():
-    print("|████████████████████████████████|")
+    print("████████████████████████████████|")
     print("SETTINGS ALL DONE")
     print("->> BOT ALREADY ONLINE <<-")
 
-bot.run("OTkyMzU0MTc0MzcxMTEwOTIz.G0cEzn.k0AoRB4gbYg1UQZsehBZDiOYmznEGNVVVEXHow")
+@bot.event
+async def on_member_join(member):
+    channel = bot.get_channel(999968465517543434) #channel ID
+    await channel.send(F"@{member} has join the Server!")
+
+@bot.event
+async def on_member_leave(member):
+    channel = bot.get_channel(999968465517543434) #channel ID
+    await channel.send(F"@{member} has leave the Server!")
+
+bot.run("") #Discord bot Token
