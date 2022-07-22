32e48160392ba0dc3858a3b3da3f4a47d654d416
Dylan-W5596 <108519539+Dylan-W5596@users.noreply.github.com>
2022-07-22 17:38:20 +0800

    join and leave
    add join and leave msg

--- bot.py
+++ bot.py
@@ -1,4 +1,13 @@
-import sys
-  
-  
-print("User Current Version:-", sys.version)
+import discord
+from discord.ext import commands
+
+bot = commands.Bot(command_prefix="FR!")  #這個機器人
+
+
+@bot.event
+async def on_ready():
+    print("|████████████████████████████████|")
+    print("SETTINGS ALL DONE")
+    print("->> BOT ALREADY ONLINE <<-")
+
+bot.run("OTkyMzU0MTc0MzcxMTEwOTIz.G0cEzn.k0AoRB4gbYg1UQZsehBZDiOYmznEGNVVVEXHow")
